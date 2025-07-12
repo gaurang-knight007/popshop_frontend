f8027f7ba0a990a8f953cb6343a00dcc651ab57c
gaurang-knight007 <149990199+gaurang-knight007@users.noreply.github.com>
2025-07-12 11:04:39 +0530

    Update socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,7 +2,9 @@
 
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+//const SOCKET_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+
+const SOCKET_URL = 'https://popshop-qjw2.onrender.com';
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
